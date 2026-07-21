a5f18b9829b8d58c1fc41e1973be96a215b8721a
Sreyas Sabbani <sreyassabbani@gmail.com>
2026-07-20 21:19:47 -0400

    tap: make saterminal entry point executable

--- Formula/saterminal.rb
+++ Formula/saterminal.rb
@@ -4,12 +4,14 @@ class Saterminal < Formula
   url "https://registry.npmjs.org/saterminal/-/saterminal-0.6.8.tgz"
   sha256 "3ae5be4fcd026577fdfc34f805c9a793eb619358c6eea224add5784fa4d6f67f"
   license "MIT"
+  revision 1
 
   depends_on "bun"
 
   def install
     system "bun", "install", "--production"
     libexec.install Dir["*"]
+    chmod 0755, libexec/"src/cli/index.ts"
     (bin/"sat").write_env_script libexec/"src/cli/index.ts", PATH: Formula["bun"].opt_bin
   end
 
